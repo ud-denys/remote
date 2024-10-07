72fbdf5a48669d6df12bd93bb854fb0280905882
s34471 <s34471@pjwstk.edu.pl>
2024-10-07 08:55:43 +0200

    initial project setup

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class main {
+    public static void main(String[] args) {
+        Adder adder = new Adder();
+        System.out.println(adder.add(1,2));
+        Subtractor subtractor = new Subtractor();
+        System.out.println(subtractor.subtract(6, 3));
+    }
+}
